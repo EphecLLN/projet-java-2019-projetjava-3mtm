9af4ce5c4e0221d063cb26a555c3e9c2aab019e0
J <HE201734@students.ephec.be>
2019-11-15 15:10:01 +0100

    gold

---
+++ src/modele/Gold.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package modele;
+
+/**
+ * @author joachim
+ *
+ */
+public class Gold {
+	public int tableauN[][] = {{0,1},{0,-1},{1,1},{-1,1},{1,0},{-1,0}};
+	public int tableauB[][] = {{0,1},{0,-1},{1,0},{-1,0},{-1,-1},{1,-1}};
+	public int axex;
+	public int axey;
+	public String couleur;
+	public Gold(int axex,int axey, String couleur) {
+		this.axex = axex;
+		this.axey = axey;
+		this.couleur = couleur;
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
